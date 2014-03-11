26bc5e352180295f7def6ddda71a723063f98e01
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-03-11 20:52:19 +0000

    Complted transactions part 3

---
+++ rd_cake/Setup/Db/payu.sql
@@ -0,0 +1,28 @@
+DROP TABLE IF EXISTS `fin_payu_transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `fin_payu_transactions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `merchant_reference` varchar(64) NOT NULL,
+  `payu_reference` varchar(64) NOT NULL,
+  `transaction_type`  enum('RESERVE','FINALISE','PAYMENT','EFFECT_STAGING','CREDIT','RESERVE_CANCEL','REGISTER_LINK') DEFAULT 'PAYMENT',
+  `transaction_state` enum('NEW','PROCESSING','SUCCESSFUL','FAILED','TIMEOUT') DEFAULT 'NEW',
+  `result_code` int(11) DEFAULT NULL,
+  `result_message` varchar(255) DEFAULT NULL,
+  `display_message` varchar(255) DEFAULT NULL,
+  `merchant_user_id` varchar(255)DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `mobile` varchar(255) DEFAULT NULL,
+  `regional_id` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name`  varchar(255) DEFAULT NULL,
+  `amount_in_cents` int(11) NOT NULL,
+  `currency_code` varchar(255) DEFAULT 'ZAR',
+  `description` varchar(255) NOT NULL,
+  `product_code` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
