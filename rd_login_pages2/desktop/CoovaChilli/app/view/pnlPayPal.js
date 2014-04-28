fc5e7b1e3b7f6170b16ea1fdc74d9e31cb829dd1
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-04-28 12:58:27 +0000

    PayPal enhancements

--- rd_login_pages2/desktop/CoovaChilli/app/view/pnlPayPal.js
+++ rd_login_pages2/desktop/CoovaChilli/app/view/pnlPayPal.js
@@ -13,7 +13,9 @@ Ext.define('CoovaChilli.view.pnlPayPal', {
         var tplVoucher = Ext.create('Ext.XTemplate', [ 
             '<h3>Last purchase</h3>'+
             '<div><div class="item">Username</div><div class="value">{username}</div></div>',
-            '<div class="alternate"><div class="item">Password</div><div class="value">{password}</div></div>'    
+            '<div class="alternate"><div class="item">Password</div><div class="value">{password}</div></div>', 
+            '<div><div class="item">Profile</div><div class="value">{profile}</div></div>',
+            '<div class="alternate"><div class="item">Valid for</div><div class="value">{valid_for}</div></div>'   
         ]);
 
         me.items    = [
@@ -21,7 +23,7 @@ Ext.define('CoovaChilli.view.pnlPayPal', {
                 height      : 140,
                 tpl         : tplVoucher,
                 data        : {username: 'UN Placeholder',password: 'PW Placeholder'},
-                bodyCls     : 'sessionClass',
+                bodyCls     : 'lastPurchase',
                 hidden      : true,
                 itemId      : 'pnlPayPalFeedback'
             },
@@ -32,7 +34,7 @@ Ext.define('CoovaChilli.view.pnlPayPal', {
                     'This detail will also be emailed to you.<br>',
                     'Alternatively contact the helpdesk.<br>'
                 ],
-                bodyCls     : 'sessionClass',
+                bodyCls     : 'lastPurchase',
                 hidden      : true,
                 itemId      : 'pnlPayPalError'
             },
@@ -40,15 +42,16 @@ Ext.define('CoovaChilli.view.pnlPayPal', {
                // bodyStyle   :{"background-color":"blue"},
                 flex        : 1,
                 html        : [
-'<div><h3>Need extra time on the Internet?</h3></div>',
+'<div class="lastPurchase">',
+'<h3>Need extra time on the Internet?</h3>',
 '<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">',
 '<input type="hidden" name="cmd" value="_s-xclick">',
 '<input type="hidden" name="hosted_button_id" value="TF9RJ5MLAZEC6">',
 '<input type="hidden" name="return" value="'+window.location+'" />',
 '<input type="hidden" name="cancel_return" value="'+window.location+'" />',
-'<input type="hidden" name="notify_url" value="http://radiusdev.webage.co/cake2/rd_cake/fin_paypal_transactions/paypal_ipn.json" />',
+'<input type="hidden" name="notify_url" value="http://95.85.10.220/cake2/rd_cake/fin_paypal_transactions/paypal_ipn.json" />',
 '<table>',
-'<tr><td><input type="hidden" name="on0" value="Vouchers">Vouchers</td></tr><tr><td><select name="os0">',
+'<tr><td class="thead"><input type="hidden" name="on0" value="Vouchers">Vouchers</td></tr><tr><td><select name="os0">',
 	'<option value="2Hours">2Hours $2.00 USD</option>',
 	'<option value="8Hours">8Hours $4.00 USD</option>',
 	'<option value="24Hours">24Hours $10.00 USD</option>',
@@ -58,7 +61,8 @@ Ext.define('CoovaChilli.view.pnlPayPal', {
 '<input type="hidden" name="currency_code" value="USD">',
 '<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">',
 '<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">',
-'</form>'
+'</form>',
+'<div>'
 
                 ]
   
