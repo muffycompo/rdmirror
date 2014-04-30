72e0d47aeae89c9fedce4c859b9b07c4383ddc4e
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-04-30 14:36:35 +0000

    2nd round of PayPal tests

--- rd_login_pages2/desktop/CoovaChilli/app/view/pnlPayPal.js
+++ rd_login_pages2/desktop/CoovaChilli/app/view/pnlPayPal.js
@@ -49,23 +49,48 @@ Ext.define('CoovaChilli.view.pnlPayPal', {
 '<h3>Need extra time on the Internet?</h3>',
 '<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">',
 '<input type="hidden" name="cmd" value="_s-xclick">',
-'<input type="hidden" name="hosted_button_id" value="TF9RJ5MLAZEC6">',
+'<input type="hidden" name="hosted_button_id" value="RXZ64B24CPY6U">',
 '<input type="hidden" name="return" value="'+clean_location+'" />',
 '<input type="hidden" name="cancel_return" value="'+clean_location+'" />',
 '<input type="hidden" name="notify_url" value="http://95.85.10.220/cake2/rd_cake/fin_paypal_transactions/paypal_ipn.json" />',
 '<table>',
 '<tr><td class="thead"><input type="hidden" name="on0" value="Vouchers">Vouchers</td></tr><tr><td><select name="os0">',
-	'<option value="2Hours">2Hours $2.00 USD</option>',
-	'<option value="8Hours">8Hours $4.00 USD</option>',
-	'<option value="24Hours">24Hours $10.00 USD</option>',
-	'<option value="72Hours">72Hours $15.00 USD</option>',
+    '<option value="2Hours">2Hours £2.00 GBP</option>',
+	'<option value="8Hours">8Hours £8.00 GBP</option>',
+	'<option value="24Hours">24Hours £10.00 GBP</option>',
+	'<option value="72Hours">72Hours £15.00 GBP</option>',
+	'<option value="120Hours">120Hours £25.00 GBP</option>',
 '</select> </td></tr>',
 '</table>',
-'<input type="hidden" name="currency_code" value="USD">',
+'<input type="hidden" name="currency_code" value="GPD">',
 '<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">',
 '<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">',
 '</form>',
 '<div>'
+
+/*
+<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
+<input type="hidden" name="cmd" value="_s-xclick">
+<input type="hidden" name="hosted_button_id" value="RXZ64B24CPY6U">
+<table>
+<tr><td><input type="hidden" name="on0" value="Vouchers">Vouchers</td></tr><tr><td><select name="os0">
+	<option value="2Hours">2Hours £2.00 GBP</option>
+	<option value="8Hours">8Hours £8.00 GBP</option>
+	<option value="24Hours">24Hours £10.00 GBP</option>
+	<option value="72Hours">72Hours £15.00 GBP</option>
+	<option value="120Hours">120Hours £25.00 GBP</option>
+</select> </td></tr>
+</table>
+<input type="hidden" name="currency_code" value="GBP">
+<input type="image" src="https://www.sandbox.paypal.com/en_US/GB/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
+<img alt="" border="0" src="https://www.sandbox.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+</form>
+
+
+
+
+
+*/
 
                 ]
   
--- rd_login_pages2/mobile/CoovaChilli/app/view/cntPayPal.js
+++ rd_login_pages2/mobile/CoovaChilli/app/view/cntPayPal.js
@@ -50,17 +50,18 @@ Ext.define('CoovaChilli.view.cntPayPal', {
                     '<h3>Need extra time on the Internet?</h3>'+
                     '<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">'+
                     '<input type="hidden" name="cmd" value="_s-xclick">'+
-                    '<input type="hidden" name="hosted_button_id" value="TF9RJ5MLAZEC6">'+
+                    '<input type="hidden" name="hosted_button_id" value="RXZ64B24CPY6U">'+
                     '<input type="hidden" name="return" value="'+clean_location+'" />'+
                     '<input type="hidden" name="cancel_return" value="'+clean_location+'" />'+
                     '<input type="hidden" name="notify_url" value="http://95.85.10.220/cake2/rd_cake/fin_paypal_transactions/paypal_ipn.json" />'+
                     '<input type="hidden" name="on0" value="Vouchers">'+
                     '<div>Vouchers</div><div>'+
                     '<select name="os0">'+
-	                    '<option value="2Hours">2Hours $2.00 USD</option>'+
-	                    '<option value="8Hours">8Hours $4.00 USD</option>'+
-	                    '<option value="24Hours">24Hours $10.00 USD</option>'+
-	                    '<option value="72Hours">72Hours $15.00 USD</option>'+
+	                    '<option value="2Hours">2Hours £2.00 GBP</option>'+
+	                    '<option value="8Hours">8Hours £8.00 GBP</option>'+
+	                    '<option value="24Hours">24Hours £10.00 GBP</option>'+
+	                    '<option value="72Hours">72Hours £15.00 GBP</option>'+
+	                    '<option value="120Hours">120Hours £25.00 GBP</option>'+
                     '</select>'+
                     '</div>'+
                     '<input type="hidden" name="currency_code" value="USD">'+
