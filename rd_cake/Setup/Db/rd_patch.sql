7ea730bc4bce7494d56a47239ec9b96279823724
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-05-22 00:21:06 +0000

    Add extra functionality to Dynamic Login Pages 1

--- rd_cake/Setup/Db/rd_patch.sql
+++ rd_cake/Setup/Db/rd_patch.sql
@@ -5,3 +5,4 @@ alter table users add column `perc_time_used` int(6) DEFAULT NULL;
 alter table users add column `perc_data_used` int(6) DEFAULT NULL;
 alter table devices add column `perc_time_used` int(6) DEFAULT NULL;
 alter table devices add column `perc_data_used` int(6) DEFAULT NULL;
+
