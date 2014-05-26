b4888d52caef675fd2a867b338230a95113630d0
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-05-26 14:25:09 +0000

    Some easy changable values for login pages

--- rd_cake/Config/RadiusDesk.php
+++ rd_cake/Config/RadiusDesk.php
@@ -74,12 +74,12 @@ $config['user_settings']['map']['lng']      = -71.0955740216735;
 $config['user_settings']['map']['lat']      = 42.3379770178396;
 
 //The location of the mobile and desktop login pages for CoovaChilli
-$config['CoovaDynamicLogin']['mobile']      = '/rd_login_pages/mobile/CoovaChilli/index.html';
-//$config['CoovaDynamicLogin']['mobile']      = '/rd_login_pages2/mobile/CoovaChilli/index.html';
+//$config['CoovaDynamicLogin']['mobile']      = '/rd_login_pages/mobile/CoovaChilli/index.html';
+$config['CoovaDynamicLogin']['mobile']      = '/rd_login_pages2/mobile/CoovaChilli/index.html';
 //$config['CoovaDynamicLogin']['mobile']      = '/rd_login_pages2/mobile/CoovaChilli/build/production/CoovaChilli/index.html';
 
-$config['CoovaDynamicLogin']['desktop']     = '/rd_login_pages/desktop/CoovaChilli/build/CoovaLogin/production/index.html';
-//$config['CoovaDynamicLogin']['desktop']     = '/rd_login_pages2/desktop/CoovaChilli/index.html';
+//$config['CoovaDynamicLogin']['desktop']     = '/rd_login_pages/desktop/CoovaChilli/build/CoovaLogin/production/index.html';
+$config['CoovaDynamicLogin']['desktop']     = '/rd_login_pages2/desktop/CoovaChilli/index.html';
 //$config['CoovaDynamicLogin']['desktop']     = '/rd_login_pages2/desktop/CoovaChilli/build/production/CoovaChilli/index.html';
 
 //The location of the mobile and desktop login pages for Mikrotik
