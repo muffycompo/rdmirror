015f85fba053cf767b7ebd01774ba6275c7aa8f6
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-06-24 23:12:07 +0000

    Work on MESH views part one - Entry Points 6

--- rd/app/model/mMeshViewEntry.js
+++ rd/app/model/mMeshViewEntry.js
@@ -1,11 +1,16 @@
 Ext.define('Rd.model.mMeshViewEntry', {
     extend: 'Ext.data.Model',
     fields: [
-         {name: 'id',               type: 'int'     },
-         {name: 'name',             type: 'string'  },
-         {name: 'encryption',       type: 'string'  },
-         {name: 'hidden',           type: 'bool'},
-         {name: 'isolate',          type: 'bool'},
-         {name: 'apply_to_all',     type: 'bool'}
+        {name: 'id',            type: 'int'     },
+        {name: 'name',          type: 'string'  },
+        {name: 'mesh_entry_id'},
+        {name: 'mac'},
+        {name: 'vendor'},
+        {name: 'tx_bytes',      type: 'int' },
+        {name: 'rx_bytes',      type: 'int' },
+        {name: 'signal_avg' },
+        {name: 'signal' },
+        {name: 'tx_bitrate',    type: 'int' },
+        {name: 'rx_bitrate',    type: 'int' }
         ]
 });
--- rd/app/view/meshes/gridMeshViewEntries.js
+++ rd/app/view/meshes/gridMeshViewEntries.js
@@ -11,15 +11,27 @@ Ext.define('Rd.view.meshes.gridMeshViewEntries' ,{
     },
     tbar: [
         { xtype: 'buttongroup', title: i18n('sAction'), items : [
-            { xtype: 'button',  iconCls: 'b-reload',    glyph: Rd.config.icnReload ,scale: 'large', itemId: 'reload',   tooltip:    i18n('sReload')}
+            { xtype: 'button',  iconCls: 'b-reload',    glyph: Rd.config.icnReload ,scale: 'large', itemId: 'reload',   tooltip:    i18n('sReload')},
+            { xtype: 'button', text: 'Past hour',    toggleGroup: 'time', enableToggle : true, scale: 'large', pressed: true},
+            { xtype: 'button', text: 'Past day',     toggleGroup: 'time', enableToggle : true, scale: 'large',},
+            { xtype: 'button', text: 'Past week',    toggleGroup: 'time', enableToggle : true, scale: 'large', },
         ]}    
     ],
     bbar: [
         {   xtype: 'component', itemId: 'count',   tpl: i18n('sResult_count_{count}'),   style: 'margin-right:5px', cls: 'lblYfi' }
     ],
+    features: [{
+        //ftype: 'grouping',
+        ftype               : 'groupingsummary',
+        groupHeaderTpl      : '{name}',
+        hideGroupedHeader   : true,
+        enableGroupingMenu  : false,
+        startCollapsed      : true
+    }],
     initComponent: function(){
         var me      = this;
         me.store    = Ext.create(Rd.store.sMeshViewEntries,{
+            groupField: 'name',
             listeners: {
                 load: function(store, records, successful) {
                     if(!successful){
@@ -65,11 +77,80 @@ Ext.define('Rd.view.meshes.gridMeshViewEntries' ,{
         me.columns  = [
             { xtype: 'rownumberer',                                                         stateId: 'StateGridMeshViewEntries1'},
             { text: i18n("sSSID"),      dataIndex: 'name',      tdCls: 'gridTree', flex: 1, stateId: 'StateGridMeshViewEntries2'},     
-            { text: 'MAC Address',      dataIndex: 'mac',       tdCls: 'gridTree', flex: 1, stateId: 'StateGridMeshViewEntries3'},
-            { text: 'Data Tx',          dataIndex: 'tx_bytes',  tdCls: 'gridTree', flex: 1, stateId: 'StateGridMeshViewEntries4'},
-            { text: 'Data Rx',          dataIndex: 'rx_bytes',  tdCls: 'gridTree', flex: 1, stateId: 'StateGridMeshViewEntries5'},
-            { text: 'Signal avg',       dataIndex: 'signal_avg',tdCls: 'gridTree', flex: 1, stateId: 'StateGridMeshViewEntries6'},
-            { text: 'Signal now',       dataIndex: 'signal',    tdCls: 'gridTree', flex: 1, stateId: 'StateGridMeshViewEntries7'}
+            { text: 'MAC Address',      dataIndex: 'mac',       tdCls: 'gridTree', flex: 1, stateId: 'StateGridMeshViewEntries3',
+                summaryType     : 'count',
+                summaryRenderer : function(value, summaryData, dataIndex) {
+                    
+                    var tx_bytes =summaryData.record.get('tx_bytes'); //Assume that if the tx_bytes are zero - we have no devices
+                    if(tx_bytes == 0){
+                        return 'No devices';
+                    }else{
+                        return ((value === 0 || value > 1) ? '(' + value + ' Devices)' : '(1 Device)');
+                    }
+                }
+            },
+            {   text: 'Data Tx',        dataIndex: 'tx_bytes',  tdCls: 'gridTree', flex: 1, stateId: 'StateGridMeshViewEntries4',
+                renderer    : function(value){
+                    return Ext.ux.bytesToHuman(value)              
+                },
+                summaryType: 'sum',
+                summaryRenderer : function(value){
+                    return Ext.ux.bytesToHuman(value)
+                }
+            },
+            {   text: 'Data Rx',        dataIndex: 'rx_bytes',  tdCls: 'gridTree', flex: 1, stateId: 'StateGridMeshViewEntries5',
+                renderer    : function(value){
+                    return Ext.ux.bytesToHuman(value)              
+                },
+                summaryType: 'sum',
+                summaryRenderer : function(value){
+                    return Ext.ux.bytesToHuman(value)
+                }
+            },
+            {   text: 'Signal avg',       dataIndex: 'signal_avg',tdCls: 'gridTree', stateId: 'StateGridMeshViewEntries6',
+                width: 150,
+                renderer: function (v, m, r) {
+                    var v = 50
+                    if(v != null){
+                        var id = Ext.id();
+                        Ext.defer(function () {
+                            Ext.widget('progressbar', {
+                                renderTo: id,
+                                value: v / 100,
+                                width: 140,
+                                text: v+" %",
+                                cls :'wifigreen'
+                            });
+                        }, 50);
+                        return Ext.String.format('<div id="{0}"></div>', id);
+                    }else{
+                        return "N/A";
+                    }
+                }
+            },
+            {   text: 'Signal now',       dataIndex: 'signal',    tdCls: 'gridTree', stateId: 'StateGridMeshViewEntries7',
+                width: 150,
+                renderer: function (v, m, r) {
+                    var v = 50
+                    if(v != null){
+                        var id = Ext.id();
+                        Ext.defer(function () {
+                            Ext.widget('progressbar', {
+                                renderTo: id,
+                                value: v / 100,
+                                width: 140,
+                                text: v+" %",
+                                cls :'wifiyellow'
+                            });
+                        }, 50);
+                        return Ext.String.format('<div id="{0}"></div>', id);
+                    }else{
+                        return "N/A";
+                    }
+                }
+
+
+            }
         ];
         me.callParent(arguments);
     }
