4b7fe4c9a9faead3f0d9a9b5aff751d4fd83baca
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-07-28 12:36:34 +0000

    Further work on the mesh connections canvas 2

--- rd_cake/Setup/Db/rd_meshdesk.sql
+++ rd_cake/Setup/Db/rd_meshdesk.sql
@@ -15,6 +15,22 @@ end//
 delimiter ;
 call meshdesk_addcolumns;
 
+DROP TABLE IF EXISTS `node_neighbors`;
+CREATE TABLE `node_neighbors` (
+  `id`                  int(11) NOT NULL AUTO_INCREMENT,
+  `node_id`             int(11) DEFAULT NULL,
+  `gateway`             enum('yes','no') DEFAULT 'no',
+  `neighbor_id`     	int(11) DEFAULT NULL,
+  `metric`              decimal(6,4) NOT NULL,
+  `created`             datetime NOT NULL,
+  `modified`            datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+
 
 
 
