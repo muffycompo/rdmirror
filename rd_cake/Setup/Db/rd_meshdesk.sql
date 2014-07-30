d7980c0ceb067c052ba104dad5bb6146c9c5fcf7
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-07-30 15:42:57 +0000

    Further work on Google Maps integration with MESHdesk 4

--- rd_cake/Setup/Db/rd_meshdesk.sql
+++ rd_cake/Setup/Db/rd_meshdesk.sql
@@ -28,6 +28,22 @@ CREATE TABLE `node_neighbors` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
+DROP TABLE IF EXISTS `mesh_specifics`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mesh_specifics` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mesh_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
 
 
 
