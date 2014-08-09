9794590fec73e3c29298f44f24bbd75785bf848b
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-08-09 03:28:01 +0000

    Modify Batman-adv configuration options 2

--- rd_cake/Setup/Db/rd_meshdesk.sql
+++ rd_cake/Setup/Db/rd_meshdesk.sql
@@ -57,6 +57,29 @@ CREATE TABLE `node_actions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `mesh_settings`
+--
+
+DROP TABLE IF EXISTS `mesh_settings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mesh_settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `mesh_id` int(11) DEFAULT NULL,
+  `aggregated_ogms` tinyint(1) NOT NULL DEFAULT '1',
+  `ap_isolation` tinyint(1) NOT NULL DEFAULT '0',
+  `bonding` tinyint(1) NOT NULL DEFAULT '0',
+  `bridge_loop_avoidance` tinyint(1) NOT NULL DEFAULT '0',
+  `fragmentation` tinyint(1) NOT NULL DEFAULT '1',
+  `distributed_arp_table` tinyint(1) NOT NULL DEFAULT '1',
+  `orig_interval` int(10) NOT NULL DEFAULT '1000',
+  `gw_sel_class` int(10) NOT NULL DEFAULT '20',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 
 
