a2bf6d44caface8390c38b66841ab371b37d8eb8
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-10-11 05:26:01 +0000

    Add Authorize.Net support 3

---
+++ rd_cake/Model/FinAuthorizeNetTransaction.php
@@ -0,0 +1,26 @@
+<?php
+App::uses('AppModel', 'Model');
+
+class FinAuthorizeNetTransaction extends AppModel {
+
+    public $name        = 'FinAuthorizeNetTransaction';
+    public $actsAs      = array('Containable');
+
+    public $belongsTo = array(
+        'User' => array(
+            'className'     => 'User',
+			'foreignKey'    => 'user_id'
+        ),
+        'Voucher'   => array(
+            'className'     => 'Voucher',
+			'foreignKey'    => 'voucher_id'
+        )
+	);
+
+    public $hasMany = array(
+        'FinPremiumSmsTransactionNote'  => array(
+            'dependent'     => true   
+        )
+    );
+}
+?>
