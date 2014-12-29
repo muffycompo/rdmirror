3d7fee8eaf7cd9d44201a9ff2fe2676e158c1974
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2014-12-29 04:27:41 +0000

    User registration part7-2

--- rd_cake/Config/RegisterUsers.php
+++ rd_cake/Config/RegisterUsers.php
@@ -7,6 +7,7 @@
 $config['register_users']['default']['creator']     = "root";
 $config['register_users']['default']['realm']       = "Residence Inn";
 $config['register_users']['default']['profile']     = "Data-Standard-250";
-$config['register_users']['default']['force_mac']   = false;
+$config['register_users']['default']['force_mac']   = true;
+$config['register_users']['default']['send_email']  = false;
 
 ?>
