e4fa02fd35079b220482ee5eefaf3a1efb8b3665
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2015-02-28 15:00:40 +0000

    Further improvements to Voucher PDF exports 1

---
+++ rd_cake/Config/Vouchers.php
@@ -0,0 +1,16 @@
+<?php
+
+//===== Vouchers ======
+
+//== Defaults for the export to pdf options ==
+$config['voucher_dafaults']['orientation']		= 'P';
+$config['voucher_dafaults']['language']			= '4_4';
+$config['voucher_dafaults']['format']			= 'a4';
+$config['voucher_dafaults']['q_r']				= true;
+$config['voucher_dafaults']['date']				= true;
+$config['voucher_dafaults']['social_media']		= true;
+$config['voucher_dafaults']['profile_detail']	= true;
+$config['voucher_dafaults']['realm_detail']		= true;
+
+
+?>
