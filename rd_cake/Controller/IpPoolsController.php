408dfbdb6cd6c01b83636b6442bc89d94e88ac4c
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2015-04-13 14:22:33 +0000

    Further work on IP Pools finishing CRUD 2

--- rd_cake/Controller/IpPoolsController.php
+++ rd_cake/Controller/IpPoolsController.php
@@ -66,28 +66,166 @@ class IpPoolsController extends AppController {
             '_serialize' => array('items','success','totalCount')
         ));
     }
+
+	public function list_of_pools(){
+
+		if(!$this->Aa->admin_check($this)){   //Only for admin users!
+            return;
+        }
+		$items 	= array();
+		$this->IpPool->contain();
+		$q_r 	= $this->IpPool->find('all',array('fields' => array('DISTINCT IpPool.pool_name')));
+
+		foreach($q_r as $i){
+			$data 			= array();
+			$data['name']	= $i['IpPool']['pool_name'];
+			$data['id']		= $i['IpPool']['pool_name'];
+			array_push($items,$data);
+		}
+
+		$this->set(array(
+            'items' 		=> $items,
+            'success' 		=> true,
+            '_serialize' => array('items','success')
+        ));
+	}
  
-    public function add() {
+    public function add_pool() {
 
         if(!$this->Aa->admin_check($this)){   //Only for admin users!
             return;
         }
 
-        $this->set(array(
+		$count  = 0;
+		$junk_trigger = 300; //We limit the trigger to 300 to prevent the user from creating havoc
+
+		//print_r($this->request->data);
+		
+
+		$this->{$this->modelClass}->create();
+
+		//Start with the first IP
+		$name 		= $this->request->data['name'];
+		$current_ip	= $this->request->data['pool_start'];
+		$end_ip		= $this->request->data['pool_end'];
+		$next_ip	= $this->_get_next_ip($current_ip);
+
+		while($current_ip != $end_ip){
+			$count++;
+			if($count > $junk_trigger){
+				$this->set(array(
+		            'success'   => false,
+		            'message'   => array('message' => "Could not add pool - Recheck range"),
+		            '_serialize' => array('success','message')
+		        ));
+				return;
+			}
+
+			$data 						= array();
+			$data['pool_name'] 			= $name;
+			$data['framedipaddress'] 	= $current_ip;
+
+			$count 	= $this->{$this->modelClass}->find('count', 
+				array('conditions' => array('IpPool.pool_name' => $name, 'IpPool.framedipaddress' => $current_ip))
+			);
+			if($count ==0){ //If already there we silently ignore it...
+				$this->{$this->modelClass}->save($data);
+				$this->{$this->modelClass}->id = null;
+			}
+			$current_ip = $next_ip;
+			$next_ip	= $this->_get_next_ip($current_ip);
+		}
+
+		//Last one in the range
+		if($current_ip == $end_ip){
+			$data 						= array();
+			$data['pool_name'] 			= $name;
+			$data['framedipaddress'] 	= $current_ip;
+
+			$count 	= $this->{$this->modelClass}->find('count', 
+				array('conditions' => array('IpPool.pool_name' => $name, 'IpPool.framedipaddress' => $current_ip))
+			);
+			if($count ==0){ //If already there we silently ignore it...
+				$this->{$this->modelClass}->save($data);
+				$this->{$this->modelClass}->id = null;
+			}
+		}
+
+		$this->set(array(
             'success' => true,
             '_serialize' => array('success')
         ));
 	}
 
-    public function delete($id = null) {
-		if(!$this->Aa->admin_check($this)){   //Only for admin users!
+	public function add_ip() {
+
+        if(!$this->Aa->admin_check($this)){   //Only for admin users!
             return;
         }
+
+		//First check so we don't add doubles
+		$name  	= $this->request->data['name'];
+		$ip		= $this->request->data['ip'];
+		
+
+		$count 	= $this->{$this->modelClass}->find('count', array('conditions' => array('IpPool.pool_name' => $name, 'IpPool.framedipaddress' => $ip)));
+		if($count > 0){
+			$this->set(array(
+	            'success'   => false,
+	            'message'   => array('message' => "IP Already listed"),
+	            '_serialize' => array('success','message')
+	        ));	
+
+		}else{
+
+			$data 						= array();
+			$data['pool_name'] 			= $name;
+			$data['framedipaddress'] 	= $ip;
+			$this->{$this->modelClass}->save($data);
+
+			$this->set(array(
+		        'success' => true,
+		        '_serialize' => array('success')
+		    ));
+		} 
+	}
+
+    public function delete($id = null) {
+
+		if(!$this->Aa->admin_check($this)){   //Only for admin users!
+            return;
+       	}
  
-        $this->set(array(
-            'success' => true,
-            '_serialize' => array('success')
-        ));
+       	if (!$this->request->is('post')) {
+			throw new MethodNotAllowedException();
+		}
+
+        $fail_flag = false;
+
+	    if(isset($this->data['id'])){   //Single item delete
+            $message = "Single item ".$this->data['id'];
+            $this->{$this->modelClass}->id = $this->data['id'];
+            $this->{$this->modelClass}->delete($this->{$this->modelClass}->id, true);
+   
+        }else{                          //Assume multiple item delete
+            foreach($this->data as $d){
+                $this->{$this->modelClass}->id = $d['id'];
+                $this->{$this->modelClass}->delete($this->{$this->modelClass}->id, true);
+            }
+        }
+
+        if($fail_flag == true){
+            $this->set(array(
+                'success'   => false,
+                'message'   => array('message' => __('Could not delete some items')),
+                '_serialize' => array('success','message')
+            ));
+        }else{
+            $this->set(array(
+                'success' => true,
+                '_serialize' => array('success')
+            ));
+        }
 	}
 
     public function view(){
@@ -201,5 +339,24 @@ class IpPoolsController extends AppController {
         //====== END REQUEST FILTER ====    
         return $c;
 	}
+
+	private function _get_next_ip($ip){
+
+        $pieces     = explode('.',$ip);
+        $octet_1    = $pieces[0];
+        $octet_2    = $pieces[1];
+        $octet_3    = $pieces[2];
+        $octet_4    = $pieces[3];
+
+        if($octet_4 >= 254){
+            $octet_4 = 1;
+            $octet_3 = $octet_3 +1;
+        }else{
+
+            $octet_4 = $octet_4 +1;
+        }
+        $next_ip = $octet_1.'.'.$octet_2.'.'.$octet_3.'.'.$octet_4;
+        return $next_ip;
+    }
     
 }
