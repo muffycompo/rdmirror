cfce57796f7f789abf132d25f088b5783d076695
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2015-09-08 09:54:10 +0000

    Do some bugfixes to Social Logins 2

--- rd_cake/Model/SocialLoginUser.php
+++ rd_cake/Model/SocialLoginUser.php
@@ -5,6 +5,12 @@ class SocialLoginUser extends AppModel {
 
     public $name        = 'SocialLoginUser';
     public $actsAs      = array('Containable');
+    
+     public $hasMany = array(
+        'SocialLoginUserRealm' => array(
+            'dependent'     => true   
+        ) 
+    );
 
 }
 ?>
