ee1dfdc8d648661598e9d4f1a4d373ae45904c41
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2016-01-08 21:14:40 +0000

    Change to Webix Dynamic Login Pages

--- rd_cake/Config/DynamicLogin.php
+++ rd_cake/Config/DynamicLogin.php
@@ -1,31 +1,31 @@
 <?php
 
 $config['DynamicLogin']['theme']['Default'] = array(
-	'coova_desktop'		=> '/rd_login_pages/desktop/CoovaChilli/build/production/CoovaChilli/index.html',
-	'coova_mobile'		=> '/rd_login_pages/mobile/CoovaChilli/build/production/CoovaChilli/index.html',
-	'mikrotik_desktop'	=> '/rd_login_pages/desktop/Mikrotik/build/production/Mikrotik/index.html',
-	'mikrotik_mobile'	=> '/rd_login_pages/mobile/Mikrotik/build/production/Mikrotik/index.html',
+	'coova_desktop'		=> '/rd_login/cc/d/index.html',
+	'coova_mobile'		=> '/rd_login/cc/m/index.html',
+	'mikrotik_desktop'	=> '/rd_login/mt/d/index.html',
+	'mikrotik_mobile'	=> '/rd_login/mt/m/index.html',
 );
 
 $config['DynamicLogin']['theme']['Green'] = array(
-	'coova_desktop'		=> '/rd_login_pages/desktop/CoovaChilli/build/production/CoovaChilli/index_green.html',
-	'coova_mobile'		=> '/rd_login_pages/mobile/CoovaChilli/build/production/CoovaChilli/index_green.html',
-	'mikrotik_desktop'	=> '/rd_login_pages/desktop/Mikrotik/build/production/Mikrotik/index_green.html',
-	'mikrotik_mobile'	=> '/rd_login_pages/mobile/Mikrotik/build/production/Mikrotik/index_green.html',
+	'coova_desktop'		=> '/rd_login/cc/d/index_green.html',
+	'coova_mobile'		=> '/rd_login/cc/m/index_green.html',
+	'mikrotik_desktop'	=> '/rd_login/mt/d/index_green.html',
+	'mikrotik_mobile'	=> '/rd_login/mt/m/index_green.html',
 );
 
 $config['DynamicLogin']['theme']['Black'] = array(
-	'coova_desktop'		=> '/rd_login_pages/desktop/CoovaChilli/build/production/CoovaChilli/index_black.html',
-	'coova_mobile'		=> '/rd_login_pages/mobile/CoovaChilli/build/production/CoovaChilli/index_black.html',
-	'mikrotik_desktop'	=> '/rd_login_pages/desktop/Mikrotik/build/production/Mikrotik/index_black.html',
-	'mikrotik_mobile'	=> '/rd_login_pages/mobile/Mikrotik/build/production/Mikrotik/index_black.html',
+	'coova_desktop'		=> '/rd_login/cc/d/index_black.html',
+	'coova_mobile'		=> '/rd_login/cc/m/index_black.html',
+	'mikrotik_desktop'	=> '/rd_login/mt/d/index_black.html',
+	'mikrotik_mobile'	=> '/rd_login/mt/m/index_black.html',
 );
 
 $config['DynamicLogin']['theme']['Grey'] = array(
-	'coova_desktop'		=> '/rd_login_pages/desktop/CoovaChilli/build/production/CoovaChilli/index_grey.html',
-	'coova_mobile'		=> '/rd_login_pages/mobile/CoovaChilli/build/production/CoovaChilli/index_grey.html',
-	'mikrotik_desktop'	=> '/rd_login_pages/desktop/Mikrotik/build/production/Mikrotik/index_grey.html',
-	'mikrotik_mobile'	=> '/rd_login_pages/mobile/Mikrotik/build/production/Mikrotik/index_grey.html',
+	'coova_desktop'		=> '/rd_login/cc/d/index_grey.html',
+	'coova_mobile'		=> '/rd_login/cc/m/index_grey.html',
+	'mikrotik_desktop'	=> '/rd_login/mt/d/index_grey.html',
+	'mikrotik_mobile'	=> '/rd_login/mt/m/index_grey.html',
 );
 
 
