ba5a2d4ef5e0dd58dfa0f4c9cd4384c00a019902
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2016-06-27 02:55:16 +0000

    Add Default Language option for login pages 2

--- rd_cake/Config/DynamicLogin.php
+++ rd_cake/Config/DynamicLogin.php
@@ -47,5 +47,9 @@ $config['DynamicLogin']['theme']['Grey'] = array(
 
 $config['DynamicLogin']['ruckus']['northbound']['password'] = 'stayoutnow123!';
 
+$config['DynamicLogin']['i18n'][0]     = array('name' => 'English',     'id' => 'en_GB',   'active' => true);
+$config['DynamicLogin']['i18n'][1]     = array('name' => 'Spanish',     'id' => 'af_ZA',   'active' => true);
+$config['DynamicLogin']['i18n'][2]     = array('name' => 'Afrikaans',   'id' => 'es_ES',   'active' => true);
+
 
 ?>
