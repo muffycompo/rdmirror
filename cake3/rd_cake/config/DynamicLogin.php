5d4e71dc1774707722387060aa0aa9fcf99fbf6e
Mfawa Alfred Onen <muffycompoqm@gmail.com>
2017-05-19 13:31:50 +0000

    New Coova Chilli Login Pages 1

--- cake3/rd_cake/config/DynamicLogin.php
+++ cake3/rd_cake/config/DynamicLogin.php
@@ -8,48 +8,18 @@ $config['DynamicLogin']['theme']['Custom'] = array(
 	
 );
 
-
 $config['DynamicLogin']['theme']['Default'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index.html',
+	'coova_desktop'		=> '/login/cp/index.html',
+	'coova_mobile'		=> '/login/cp/index.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index.html',
 	'ruckus_mobile'     => '/rd_login/ru/m/index.html',
 );
 
-
-$config['DynamicLogin']['theme']['Green'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_green.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_green.html',
-	'mikrotik_desktop'	=> '/rd_login/mt/d/index_green.html',
-	'mikrotik_mobile'	=> '/rd_login/mt/m/index_green.html',
-	'ruckus_desktop'    => '/rd_login/ru/d/index_green.html',
-	'ruckus_mobile'     => '/rd_login/ru/m/index_green.html',
-);
-
-$config['DynamicLogin']['theme']['Black'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_black.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_black.html',
-	'mikrotik_desktop'	=> '/rd_login/mt/d/index_black.html',
-	'mikrotik_mobile'	=> '/rd_login/mt/m/index_black.html',
-	'ruckus_desktop'    => '/rd_login/ru/d/index_black.html',
-	'ruckus_mobile'     => '/rd_login/ru/m/index_black.html',
-);
-
-$config['DynamicLogin']['theme']['Grey'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_grey.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_grey.html',
-	'mikrotik_desktop'	=> '/rd_login/mt/d/index_grey.html',
-	'mikrotik_mobile'	=> '/rd_login/mt/m/index_grey.html',
-	'ruckus_desktop'    => '/rd_login/ru/d/index_grey.html',
-	'ruckus_mobile'     => '/rd_login/ru/m/index_grey.html',
-);
-
-/*
 $config['DynamicLogin']['theme']['Lightgrey Red Green'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_lightgrey_red_green.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_lightgrey_red_green.html',
+	'coova_desktop'		=> '/login/cp/index_lightgrey_red_green.html',
+	'coova_mobile'		=> '/login/cp/index_lightgrey_red_green.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_lightgrey_red_green.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_lightgrey_red_green.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_lightgrey_red_green.html',
@@ -57,8 +27,8 @@ $config['DynamicLogin']['theme']['Lightgrey Red Green'] = array(
 );
 
 $config['DynamicLogin']['theme']['Lightgrey Blue Green'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_lightgrey_blue_green.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_lightgrey_blue_green.html',
+	'coova_desktop'		=> '/login/cp/index_lightgrey_blue_green.html',
+	'coova_mobile'		=> '/login/cp/index_lightgrey_blue_green.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_lightgrey_blue_green.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_lightgrey_blue_green.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_lightgrey_blue_green.html',
@@ -66,8 +36,8 @@ $config['DynamicLogin']['theme']['Lightgrey Blue Green'] = array(
 );
 
 $config['DynamicLogin']['theme']['Darkgrey Blue Green'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_darkgrey_blue_green.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_darkgrey_blue_green.html',
+	'coova_desktop'		=> '/login/cp/index_darkgrey_blue_green.html',
+	'coova_mobile'		=> '/login/cp/index_darkgrey_blue_green.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_darkgrey_blue_green.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_darkgrey_blue_green.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_darkgrey_blue_green.html',
@@ -75,8 +45,8 @@ $config['DynamicLogin']['theme']['Darkgrey Blue Green'] = array(
 );
 
 $config['DynamicLogin']['theme']['Darkgrey Red Green'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_darkgrey_red_green.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_darkgrey_red_green.html',
+	'coova_desktop'		=> '/login/cp/index_darkgrey_red_green.html',
+	'coova_mobile'		=> '/login/cp/index_darkgrey_red_green.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_darkgrey_red_green.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_darkgrey_red_green.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_darkgrey_red_green.html',
@@ -84,8 +54,8 @@ $config['DynamicLogin']['theme']['Darkgrey Red Green'] = array(
 );
 
 $config['DynamicLogin']['theme']['Black Blue and Red'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_black_blue_red.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_black_blue_red.html',
+	'coova_desktop'		=> '/login/cp/index_black_blue_red.html',
+	'coova_mobile'		=> '/login/cp/index_black_blue_red.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_black_blue_red.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_black_blue_red.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_black_blue_red.html',
@@ -93,8 +63,8 @@ $config['DynamicLogin']['theme']['Black Blue and Red'] = array(
 );
 
 $config['DynamicLogin']['theme']['Black Red Green'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_black_red_green.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_black_red_green.html',
+	'coova_desktop'		=> '/login/cp/index_black_red_green.html',
+	'coova_mobile'		=> '/login/cp/index_black_red_green.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_black_red_green.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_black_red_green.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_black_red_green.html',
@@ -102,8 +72,8 @@ $config['DynamicLogin']['theme']['Black Red Green'] = array(
 );
 
 $config['DynamicLogin']['theme']['Black Aqua Green'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_black_aqua_green.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_black_aqua_green.html',
+	'coova_desktop'		=> '/login/cp/index_black_aqua_green.html',
+	'coova_mobile'		=> '/login/cp/index_black_aqua_green.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_black_aqua_green.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_black_aqua_green.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_black_aqua_green.html',
@@ -111,8 +81,8 @@ $config['DynamicLogin']['theme']['Black Aqua Green'] = array(
 );
 
 $config['DynamicLogin']['theme']['Navy Lime Red'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_navy_lime_red.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_navy_lime_red.html',
+	'coova_desktop'		=> '/login/cp/index_navy_lime_red.html',
+	'coova_mobile'		=> '/login/cp/index_navy_lime_red.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_navy_lime_red.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_navy_lime_red.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_navy_lime_red.html',
@@ -120,8 +90,8 @@ $config['DynamicLogin']['theme']['Navy Lime Red'] = array(
 );
 
 $config['DynamicLogin']['theme']['Navy Red Green'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_navy_red_green.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_navy_red_green.html',
+	'coova_desktop'		=> '/login/cp/index_navy_red_green.html',
+	'coova_mobile'		=> '/login/cp/index_navy_red_green.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_navy_red_green.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_navy_red_green.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_navy_red_green.html',
@@ -129,8 +99,8 @@ $config['DynamicLogin']['theme']['Navy Red Green'] = array(
 );
 
 $config['DynamicLogin']['theme']['Red Green Blue'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_red_green_blue.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_red_green_blue.html',
+	'coova_desktop'		=> '/login/cp/index_red_green_blue.html',
+	'coova_mobile'		=> '/login/cp/index_red_green_blue.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_red_green_blue.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_red_green_blue.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_red_green_blue.html',
@@ -138,8 +108,8 @@ $config['DynamicLogin']['theme']['Red Green Blue'] = array(
 );
 
 $config['DynamicLogin']['theme']['Red Blue Grey'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_red_blue_grey.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_red_blue_grey.html',
+	'coova_desktop'		=> '/login/cp/index_red_blue_grey.html',
+	'coova_mobile'		=> '/login/cp/index_red_blue_grey.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_red_blue_grey.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_red_blue_grey.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_red_blue_grey.html',
@@ -156,8 +126,8 @@ $config['DynamicLogin']['theme']['Orange Blue Grey'] = array(
 );
 
 $config['DynamicLogin']['theme']['Orange Blue Green'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_orange_blue_green.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_orange_blue_green.html',
+	'coova_desktop'		=> '/login/cp/index_orange_blue_green.html',
+	'coova_mobile'		=> '/login/cp/index_orange_blue_green.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_orange_blue_green.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_orange_blue_green.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_orange_blue_green.html',
@@ -165,8 +135,8 @@ $config['DynamicLogin']['theme']['Orange Blue Green'] = array(
 );
 
 $config['DynamicLogin']['theme']['Blue Red Grey'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_blue_red_grey.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_blue_red_grey.html',
+	'coova_desktop'		=> '/login/cp/index_blue_red_grey.html',
+	'coova_mobile'		=> '/login/cp/index_blue_red_grey.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_blue_red_grey.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_blue_red_grey.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_blue_red_grey.html',
@@ -174,8 +144,8 @@ $config['DynamicLogin']['theme']['Blue Red Grey'] = array(
 );
 
 $config['DynamicLogin']['theme']['Blue Green Grey'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_blue_green_grey.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_blue_green_grey.html',
+	'coova_desktop'		=> '/login/cp/index_blue_green_grey.html',
+	'coova_mobile'		=> '/login/cp/index_blue_green_grey.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_blue_green_grey.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_blue_green_grey.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_blue_green_grey.html',
@@ -183,8 +153,8 @@ $config['DynamicLogin']['theme']['Blue Green Grey'] = array(
 );
 
 $config['DynamicLogin']['theme']['Winered Green Grey'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_winered_green_grey.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_winered_green_grey.html',
+	'coova_desktop'		=> '/login/cp/index_winered_green_grey.html',
+	'coova_mobile'		=> '/login/cp/index_winered_green_grey.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_winered_green_grey.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_winered_green_grey.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_winered_green_grey.html',
@@ -192,8 +162,8 @@ $config['DynamicLogin']['theme']['Winered Green Grey'] = array(
 );
 
 $config['DynamicLogin']['theme']['Woodgreen Red Grey'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_woodgreen_red_grey.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_woodgreen_red_grey.html',
+	'coova_desktop'		=> '/login/cp/index_woodgreen_red_grey.html',
+	'coova_mobile'		=> '/login/cp/index_woodgreen_red_grey.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_woodgreen_red_grey.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_woodgreen_red_grey.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_woodgreen_red_grey.html',
@@ -201,8 +171,8 @@ $config['DynamicLogin']['theme']['Woodgreen Red Grey'] = array(
 );
 
 $config['DynamicLogin']['theme']['Lime Grey Red'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_lime_grey_red.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_lime_grey_red.html',
+	'coova_desktop'		=> '/login/cp/index_lime_grey_red.html',
+	'coova_mobile'		=> '/login/cp/index_lime_grey_red.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_lime_grey_red.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_lime_grey_red.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_lime_grey_red.html',
@@ -210,8 +180,8 @@ $config['DynamicLogin']['theme']['Lime Grey Red'] = array(
 );
 
 $config['DynamicLogin']['theme']['Softpink Grey Lime'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_softpink_grey_lime.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_softpink_grey_lime.html',
+	'coova_desktop'		=> '/login/cp/index_softpink_grey_lime.html',
+	'coova_mobile'		=> '/login/cp/index_softpink_grey_lime.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_softpink_grey_lime.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_softpink_grey_lime.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_softpink_grey_lime.html',
@@ -219,8 +189,8 @@ $config['DynamicLogin']['theme']['Softpink Grey Lime'] = array(
 );
 
 $config['DynamicLogin']['theme']['Pink Blue Green'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_pink_blue_green.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_pink_blue_green.html',
+	'coova_desktop'		=> '/login/cp/index_pink_blue_green.html',
+	'coova_mobile'		=> '/login/cp/index_pink_blue_green.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_pink_blue_green.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_pink_blue_green.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_pink_blue_green.html',
@@ -228,15 +198,22 @@ $config['DynamicLogin']['theme']['Pink Blue Green'] = array(
 );
 
 $config['DynamicLogin']['theme']['Turqois Grey Red'] = array(
-	'coova_desktop'		=> '/rd_login/cc/d/index_turqois_grey_red.html',
-	'coova_mobile'		=> '/rd_login/cc/m/index_turqois_grey_red.html',
+	'coova_desktop'		=> '/login/cp/index_turqois_grey_red.html',
+	'coova_mobile'		=> '/login/cp/index_turqois_grey_red.html',
 	'mikrotik_desktop'	=> '/rd_login/mt/d/index_turqois_grey_red.html',
 	'mikrotik_mobile'	=> '/rd_login/mt/m/index_turqois_grey_red.html',
 	'ruckus_desktop'    => '/rd_login/ru/d/index_turqois_grey_red.html',
 	'ruckus_mobile'     => '/rd_login/ru/m/index_turqois_grey_red.html',
 );
 
-*/
+$config['DynamicLogin']['theme']['Softgreen'] = array(
+	'coova_desktop'		=> '/login/cp/index_softgreen.html',
+	'coova_mobile'		=> '/login/cp/index_softgreen.html',
+	'mikrotik_desktop'	=> '/rd_login/mt/d/index_turqois_grey_red.html',
+	'mikrotik_mobile'	=> '/rd_login/mt/m/index_turqois_grey_red.html',
+	'ruckus_desktop'    => '/rd_login/ru/d/index_turqois_grey_red.html',
+	'ruckus_mobile'     => '/rd_login/ru/m/index_turqois_grey_red.html',
+);
 
 
 $config['DynamicLogin']['ruckus']['northbound']['password'] = 'stayoutnow123!';
